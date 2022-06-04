4d7fd842a1d583bab0b914c0ca2728e369c60803
isaac petersen <isaac.petersen5@gmail.com>
2022-06-03 17:27:46 -0700

    woo, found the sig. need a bash script to automate though

--- dehashing/sha512hash.py
+++ dehashing/sha512hash.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import hashlib
-import itertools
+
 def encode():
     match_this = "3052cfcc32d0c7c47138649872f3d7ef74413330a8dc1b29c9d21c991dc4d44a19ece2f4e3ed5e3cb7bbac3a05a7624d1010fd502265e7a636960349698342e2"
     first = ["1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F"]
