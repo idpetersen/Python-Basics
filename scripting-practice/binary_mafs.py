666a99dd4a754e0586003fca6b52ded4a31466b4
isaac petersen <isaac.petersen5@gmail.com>
2022-06-14 08:40:31 -0700

    extra stuff for scripting

--- scripting-practice/binary_mafs.py
+++ scripting-practice/binary_mafs.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
+
+import sys
+
+
+
+
+def binary_mafs(file):
+    with open(file) as open_file:
+        for lines in open_file:
+            print(lines)
+ 
+
+
 def main():
-    pass
+    file = sys.argv[1]
+    binary_mafs(file)
 
 if __name__ == "__main__":
     main()
+
+
+
